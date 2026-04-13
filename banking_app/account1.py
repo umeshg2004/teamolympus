06ea2e9c904261b819d64829d692121cd38c5780
umeshg2004 <umeshgotyal2004@gmail.com>
2026-04-13 17:10:08 +0530

    Add files via upload

---
+++ banking_app/account1.py
@@ -0,0 +1,18 @@
+﻿from sqlalchemy import Column, Integer, Float, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from ..database import Base
+
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
+    balance = Column(Float, default=0.0, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    customer = relationship("Customer", back_populates="accounts")
+    transactions = relationship("Transaction", back_populates="account")
